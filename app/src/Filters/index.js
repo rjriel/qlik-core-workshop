bbce96438324d43819a9f346978617d0cd6d24ca
rjriel <rie@qlik.com>
2019-08-13 14:02:48 -0400

    7 - Clearing Selections

--- app/src/Filters/index.js
+++ app/src/Filters/index.js
@@ -1,12 +1,20 @@
 import React from "react"
 import "./Filters.css"
 import Filter from "../Filter"
+import EnigmaService from "../services/EnigmaService"
 
 const Filters = () => {
+  const clear = async () => {
+    await EnigmaService.clearSelections()
+  }
+
   return (
     <div className="Filters">
       <div className="titleContainer">
         <div className="title">Filters</div>
+        <button className="button" onClick={clear}>
+          Clear All
+        </button>
       </div>
       <Filter title="Ratings" field="content_rating" />
       <Filter title="Country" field="country" />
