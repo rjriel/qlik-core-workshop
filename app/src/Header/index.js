9cc7f471b96fbdce1dc93b7de16a568d83472e07
rjriel <rie@qlik.com>
2019-08-13 14:08:45 -0400

    8 - Adding Search

--- app/src/Header/index.js
+++ app/src/Header/index.js
@@ -2,13 +2,14 @@ import React from "react"
 import "./Header.css"
 import logo from "../assets/logo.png"
 import Filters from "../Filters"
+import Search from "../Search"
 
 const Header = () => {
   return (
     <div className="Header">
       <img className="logo" src={logo} alt="Qlik Core Films" />
       <Filters />
-      <div />
+      <Search />
     </div>
   )
 }
