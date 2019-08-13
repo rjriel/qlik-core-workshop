3ab09eba5e411c6e4a74bac9574f489ff7a70b50
rjriel <rie@qlik.com>
2019-08-13 13:54:36 -0400

    5 - creating lists

--- app/src/Header/index.js
+++ app/src/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "./Header.css"
 import logo from "../assets/logo.png"
+import Filters from "../Filters"
 
 const Header = () => {
   return (
     <div className="Header">
       <img className="logo" src={logo} alt="Qlik Core Films" />
+      <Filters />
+      <div />
     </div>
   )
 }
